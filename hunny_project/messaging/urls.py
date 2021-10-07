eb85f64287112af0ffc4f9c980369c93be711338
Kody Gentry <kmg221@txstate.edu>
2021-10-06 19:20:01 -0500

    delete unwanted files

--- hunny_project/messaging/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='messaging-home'),
-    path('room/', views.room, name='messaging-room'),
-    
-]
