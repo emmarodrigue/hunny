4baca5fd91474c53cf33da80f45651faf7cbd009
Saron Abebe <s_a383@txstate.edu>
2021-10-08 18:35:08 +0000

    urls.py edited online with Bitbucket

--- hunny/datingapp/urls.py
+++ hunny/datingapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'datingapp-home'),
     path('about/', views.about, name = 'datingapp-about'),
+    path('user/', views.user, name = 'datingapp-user'),
 ]
