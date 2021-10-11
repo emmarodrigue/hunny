8d23927643751163cf21799aaa09e5adf3393f0a
Isaac Jaramillo <i_j40@txstate.edu>
2021-10-11 01:18:24 -0500

    Login added

--- hunny_project/hunny_app/urls.py
+++ hunny_project/hunny_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='hunny-home'),
-    path('login/', views.login, name='hunny-login'),
+    path('login/', views.login_request, name='login'),
     path('profile/', views.profile, name='hunny-profile'),    
     path('messages/', views.messages, name='hunny-messages'),
     path('message/room/', views.messages_room, name='hunny-messages_room'),
--- hunny_project/hunny_app/views.py
+++ hunny_project/hunny_app/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import NewUserForm
 from django.contrib import messages
 
 def home(request):
      return render(request, 'home.html')
 
-def login(request):
-    return render(request, 'login.html')
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("/")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="logins.html", context={"login_form":form})
 
 def profile(request):
      return render(request, 'profile.html')
