a3d64b6f2321f4c8647f8eeebc542e8045ba3ad4
emma <enr39@txstate.edu>
2021-10-21 12:18:05 -0500

    added universal navbar and placeholder pages

--- hunny_project/hunny_app/urls.py
+++ hunny_project/hunny_app/urls.py
@@ -11,13 +11,20 @@ urlpatterns = [
     path('terms_service/', views.terms_service, name='hunny-terms-service'),
     path('home/', views.home, name='hunny-home'),
     path('login/', views.login, name='hunny-login'),
+    path('about/', views.about, name='hunny-about'),
+    path('contact/', views.contact, name='hunny-contact'),
+
     path('profile/', views.profile, name='hunny-profile'),    
     path('edit-profile/', views.editProfile, name='hunny-edit-profile'),
-    
+
     path('messages/', views.messages, name='hunny-messages'),
     path('messages/<str:room_name>/', views.messages_room, name='hunny-messages-room'),
     
     path('user/', views.user, name='hunny-user'),
-    path('about/', views.about, name='hunny-about'),
-    path('contact/', views.contact, name='hunny-contact'),
+    path('settings/', views.settings, name='hunny-settings'),
+
+    path('matchroom/', views.matchroom, name='hunny-matchroom'),
+    path('matchbox/', views.matchbox, name='hunny-matchbox'),
+
+    path('preferences/', views.preferences, name='hunny-preferences'),
 ]
--- hunny_project/hunny_app/views.py
+++ hunny_project/hunny_app/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth import get_user_model
 
-
 def landing(request):
      return render(request, 'landing.html')
 
@@ -37,41 +36,14 @@ def login(request):
     context = {}
     return render(request, 'login.html')
 
-def profile(request):
-     return render(request, 'profile.html')
-
-
-
-def messages(request):
-    return render(request, 'messages.html', {})
-
-def messages_room(request, room_name):
-    return render(request, 'messages_room.html', {'room_name': room_name})
-
-
-def user(request):
-    return render(request, 'user.html')
-
 def about(request):
     return render(request, 'about.html')
 
 def contact(request):
     return render(request, 'contact.html')
 
-def register(request):
-    form = CreateUserForm();
-    
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            # messages.success(request, f'Account was created for {user}')
-            
-            return redirect('hunny-login')
-        
-    context = {'form': form}
-    return render(request, 'signup.html', context)
+def profile(request):
+     return render(request, 'profile.html')
 
 @login_required
 def editProfile(request):
@@ -89,3 +61,39 @@ def editProfile(request):
     }
 
     return render(request, 'hunny_app/edit_profile.html', context)
+
+def messages(request):
+    return render(request, 'messages.html', {})
+
+def messages_room(request, room_name):
+    return render(request, 'messages_room.html', {'room_name': room_name})
+
+def user(request):
+    return render(request, 'user.html')
+
+def register(request):
+    form = CreateUserForm();
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            # messages.success(request, f'Account was created for {user}')
+            
+            return redirect('hunny-login')
+        
+    context = {'form': form}
+    return render(request, 'signup.html', context)
+
+def settings(request):
+    return render(request, 'settings.html')
+
+def matchroom(request):
+    return render(request, 'matchroom.html')
+
+def matchbox(request):
+    return render(request, 'matchbox.html')
+
+def preferences(request):
+    return render(request, 'preferences.html')
