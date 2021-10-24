a0634eef1f68c4f70e450953db46915b68e06526
emma <enr39@txstate.edu>
2021-10-24 17:34:23 -0500

    added login_required decorator and redirect to user-only pages

--- hunny_project/hunny_app/views.py
+++ hunny_project/hunny_app/views.py
@@ -2,16 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ProfileUpdateForm, CreateUserForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
 
 def landing(request):
      return render(request, 'landing.html')
 
-def matchingroom(request):
-    context = {}
-    return render(request, 'matchingroom.html', context)
-
-def accountlogin(request):
+def login(request):
      if request.method == 'POST':
          user_name = request.POST.get('username')
          user_password = request.POST.get('password')
@@ -23,71 +19,6 @@ def accountlogin(request):
              messages.info(request, "Username or Password is incorrect!")
      context = {}
      return render(request, 'login.html', context)
-
-def terms_service(request):
-     return render(request, 'terms_service.html')
-
-def accountlogout(request):
-    logout(request)
-    return redirect('hunny-login')
-
-def home(request):
-     return render(request, 'home.html')
-
-def login(request):
-    
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        
-        user = authenticate(request, username=username, password=password)
-        
-        if user is not None:
-            login(request, user)
-            redirect('hunny-initial')
-        # else:
-        #     messages.info(request, 'Username OR password is incorrect')
-    
-    context = {}
-    return render(request, 'login.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-def contact(request):
-    return render(request, 'contact.html')
-
-@login_required
-def profile(request):
-     return render(request, 'profile.html')
-
-@login_required
-def editProfile(request):
-    if request.method == 'POST':
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if p_form.is_valid():
-            p_form.save()
-            return redirect('/profile')
-    else:
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-    context = {'p_form': p_form}
-    return render(request, 'profile_edit.html', context)
-
-
-def message(request):
-    return render(request, 'messages.html', {})
-
-def messages_room(request, room_name):
-    return render(request, 'messages_room.html', {'room_name': room_name})
-
-def user(request):
-    return render(request, 'user.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-def contact(request):
-    return render(request, 'contact.html')
 
 def signup(request):
     form = CreateUserForm();
@@ -101,11 +32,58 @@ def signup(request):
     context = {'form': form}
     return render(request, 'signup.html', context)
 
+def terms_service(request):
+     return render(request, 'terms_service.html')
+
+def home(request):
+     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+@login_required(login_url='/landing')
+def profile(request):
+     return render(request, 'profile.html')
+
+@login_required(login_url='/landing')
+def editProfile(request):
+    if request.method == 'POST':
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if p_form.is_valid():
+            p_form.save()
+            return redirect('/profile')
+    else:
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {'p_form': p_form}
+    return render(request, 'profile_edit.html', context)
+
+@login_required(login_url='/landing')
+def message(request):
+    return render(request, 'messages.html', {})
+
+@login_required(login_url='/landing')
+def messages_room(request, room_name):
+    return render(request, 'messages_room.html', {'room_name': room_name})
+
+@login_required(login_url='/landing')
+def user(request):
+    return render(request, 'user.html')
+
+@login_required(login_url='/landing')
 def settings(request):
     return render(request, 'settings.html')
 
+@login_required(login_url='/landing')
 def preferences(request):
     return render(request, 'preferences.html')
+
+@login_required(login_url='/landing')
+def matchingroom(request):
+    context = {}
+    return render(request, 'matchingroom.html', context)
 
 messages = [
     {
