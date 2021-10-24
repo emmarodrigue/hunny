04eb23785ca917f24c7dc547be7537f8a6533eb8
Kody Gentry <kmg221@txstate.edu>
2021-10-24 14:55:31 -0500

    remove 10000000000 files

--- hunny_project/hunny_project/asgi.py
+++ hunny_project/hunny_project/asgi.py
@@ -1,12 +1,17 @@
-# mysite/asgi.py
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import hunny_app.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            hunny_app.routing.websocket_urlpatterns
+        )
+    ),
 })
