461ac98436f94676778971b1c90354fb6fcac93b
Isaac Jaramillo <i_j40@txstate.edu>
2021-10-24 10:23:03 -0500

    Basic changes, adding features to profile.html

--- hunny_project/hunny_app/forms.py
+++ hunny_project/hunny_app/forms.py
@@ -5,10 +5,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
+
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['first_name', 'last_name', 'city', 'age', 'age_range', 'match_radius', 'bio', 'image']
+
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
--- hunny_project/hunny_app/views.py
+++ hunny_project/hunny_app/views.py
@@ -8,53 +8,69 @@ from django.contrib.auth import authenticate, login, logout
 
 
 def landing(request):
-     return render(request, 'landing.html')
-#lilly_note
+    return render(request, 'landing.html')
+
+
+# lilly_note
 def matchingroom(request):
     context = {}
     return render(request, 'matchingroom.html', context)
 
 
-#lilly_note
+# lilly_note
 def accountlogin(request):
-     if request.method == 'POST':
-         user_name = request.POST.get('username')
-         user_password = request.POST.get('password')
-         user = authenticate(request, username=user_name, password=user_password)
-         if user is not None:
-             login(request, user)
-             return redirect('hunny-profile')
-         else:
-             messages.info(request, "Username or Password is incorrect!")
-     context = {}
-     return render(request, 'login.html', context)
+    '''
+    Takes in a request to allow proper information to get passed and log
+    the user in.
+    '''
+    if request.method == 'POST':
+        user_name = request.POST.get('username')
+        user_password = request.POST.get('password')
+        user = authenticate(request, username=user_name, password=user_password)
+        if user is not None:
+            login(request, user)
+            return redirect('hunny-profile')
+        else:
+            messages.info(request, "Username or Password is incorrect!")
+    context = {}
+    return render(request, 'login.html', context)
+
 
 def terms_service(request):
-     return render(request, 'terms_service.html',context)
+    context = {}
+    return render(request, 'terms_service.html', context)
+
 
 def accountlogout(request):
     logout(request)
     return redirect('hunny-login')
 
+
 def home(request):
-     return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def profile(request):
-     return render(request, 'profile.html')
+    return render(request, 'profile.html')
+
 
 def message(request):
     return render(request, 'messages.html')
 
+
 def user(request):
     return render(request, 'user.html')
+
 
 def about(request):
     return render(request, 'about.html')
 
+
 def contact(request):
     return render(request, 'contact.html')
 
-#lilly_note
+
+# lilly_note
 def signup(request):
     form = CreateUserForm();
     if request.method == 'POST':
@@ -62,12 +78,10 @@ def signup(request):
         if form.is_valid():
             form.save()
             user_name = form.cleaned_data.get('username')
-            messages.success(request,'Account was created successfully for ' + user_name )
+            messages.success(request, 'Account was created successfully for ' + user_name)
             return redirect('hunny-login')
     context = {'form': form}
     return render(request, 'signup.html', context)
-
-
 
 
 @login_required()
@@ -88,62 +102,61 @@ def editProfile(request):
     return render(request, 'hunny_app/edit_profile.html', context)
 
 
-
 message_list = [
     {
-    'recipient': 'Recipient',
-    'message_contents': 'Message',
-    'date_recieved': 'Date Recieved',
-    'date_delivered': 'Date Delivered',
-    'date_read': 'Date Read',
+        'recipient': 'Recipient',
+        'message_contents': 'Message',
+        'date_recieved': 'Date Recieved',
+        'date_delivered': 'Date Delivered',
+        'date_read': 'Date Read',
     },
     {
-    'recipient': 'Recipient',
-    'message_contents': 'Message',
-    'date_recieved': 'Date Recieved',
-    'date_delivered': 'Date Delivered',
-    'date_read': 'Date Read',
+        'recipient': 'Recipient',
+        'message_contents': 'Message',
+        'date_recieved': 'Date Recieved',
+        'date_delivered': 'Date Delivered',
+        'date_read': 'Date Read',
     },
     {
-    'recipient': 'Recipient',
-    'message_contents': 'Message',
-    'date_recieved': 'Date Recieved',
-    'date_delivered': 'Date Delivered',
-    'date_read': 'Date Read',
+        'recipient': 'Recipient',
+        'message_contents': 'Message',
+        'date_recieved': 'Date Recieved',
+        'date_delivered': 'Date Delivered',
+        'date_read': 'Date Read',
     },
     {
-    'recipient': 'Recipient',
-    'message_contents': 'Message',
-    'date_recieved': 'Date Recieved',
-    'date_delivered': 'Date Delivered',
-    'date_read': 'Date Read',
+        'recipient': 'Recipient',
+        'message_contents': 'Message',
+        'date_recieved': 'Date Recieved',
+        'date_delivered': 'Date Delivered',
+        'date_read': 'Date Read',
     },
-        {
-    'recipient': 'Recipient',
-    'message_contents': 'Message',
-    'date_recieved': 'Date Recieved',
-    'date_delivered': 'Date Delivered',
-    'date_read': 'Date Read',
+    {
+        'recipient': 'Recipient',
+        'message_contents': 'Message',
+        'date_recieved': 'Date Recieved',
+        'date_delivered': 'Date Delivered',
+        'date_read': 'Date Read',
     },
-        {
-    'recipient': 'Recipient',
-    'message_contents': 'Message',
-    'date_recieved': 'Date Recieved',
-    'date_delivered': 'Date Delivered',
-    'date_read': 'Date Read',
+    {
+        'recipient': 'Recipient',
+        'message_contents': 'Message',
+        'date_recieved': 'Date Recieved',
+        'date_delivered': 'Date Delivered',
+        'date_read': 'Date Read',
     },
-        {
-    'recipient': 'Recipient',
-    'message_contents': 'Message',
-    'date_recieved': 'Date Recieved',
-    'date_delivered': 'Date Delivered',
-    'date_read': 'Date Read',
+    {
+        'recipient': 'Recipient',
+        'message_contents': 'Message',
+        'date_recieved': 'Date Recieved',
+        'date_delivered': 'Date Delivered',
+        'date_read': 'Date Read',
     },
-        {
-    'recipient': 'Recipient',
-    'message_contents': 'Message',
-    'date_recieved': 'Date Recieved',
-    'date_delivered': 'Date Delivered',
-    'date_read': 'Date Read',
+    {
+        'recipient': 'Recipient',
+        'message_contents': 'Message',
+        'date_recieved': 'Date Recieved',
+        'date_delivered': 'Date Delivered',
+        'date_read': 'Date Read',
     },
 ]
