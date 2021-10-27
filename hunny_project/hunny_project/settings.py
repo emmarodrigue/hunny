72a8b1ee819b2cc0cd7d513295c63ec2909c671e
Kody Gentry <kmg221@txstate.edu>
2021-10-27 15:16:15 -0500

    add potential reqs, use Selenium to run tests on chat functionality, create test db

--- hunny_project/hunny_project/settings.py
+++ hunny_project/hunny_project/settings.py
@@ -75,13 +75,16 @@ TEMPLATES = [
 
 
 
-# Database
+# Database - @ISAAC change when switching DBs ?
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
 
