ef020ad04640adb990d18881c11df4eb4786bd44
Kody Gentry <kmg221@txstate.edu>
2021-10-31 17:33:32 -0500

    fix admin login, comment out path for now

--- hunny_project/hunny_app/models.py
+++ hunny_project/hunny_app/models.py
@@ -70,11 +70,11 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        # img = Image.open(self.image.path)
+        # if img.height > 300 or img.width > 300:
+            # output_size = (300,300)
+            # img.thumbnail(output_size)
+            # img.save(self.image.path)
 
     def next_check(self):
         super().save()
--- hunny_project/hunny_app/views.py
+++ hunny_project/hunny_app/views.py
@@ -119,9 +119,6 @@ def home(request):
 def profile(request):
      return render(request, 'profile.html')
 
-def user(request):
-    return render(request, 'user.html')
-
 def about(request):
     return render(request, 'about.html')
 
