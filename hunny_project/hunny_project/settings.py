391eb0d48c7718322ee93635b1c262212a0b31d6
Isaac Jaramillo <i_j40@txstate.edu>
2021-11-03 17:03:43 -0500

    left the default, but added google one too.

--- hunny_project/hunny_project/settings.py
+++ hunny_project/hunny_project/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'hunny_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    #     'TEST': {
-    #         'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
-    #     }
-    # }
-     'default': {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
+     },
+     'google-sql': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'users',
         'USER': 'hunny-db',
